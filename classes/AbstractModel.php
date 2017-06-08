261df20d7d636fdd61ad6b0653de5640b8c8de2e
genjo <insidia93@gmail.com>
2017-06-08 11:26:08 +0300

    Создали метод получения одной новости из базы данных

--- classes/AbstractModel.php
+++ classes/AbstractModel.php
@@ -13,4 +13,11 @@ class AbstractModel
         $sql ="SELECT * FROM " . static::$table;
         return $db->getAllDataFromDB($sql,static::class);
     }
+
+    public static function  getOne($id)
+    {
+        $db = new DB();
+        $sql = "SELECT * FROM " . static::$table . " WHERE id = " . $id;
+        return $db->getOneDataFromDB($sql,static::$class);
+    }
 }
