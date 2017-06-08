5c2d2327568f9bf9c50585ed1f30213ef0a6c34f
genjo <insidia93@gmail.com>
2017-06-08 09:30:08 +0300

    Написали метод получения новостей из бд

--- classes/MySql.php
+++ classes/MySql.php
@@ -23,5 +23,17 @@ class MySql
         return mysqli_connect($this->host,$this->root,$this->password,$this->db);
     }
 
+    public function getDataFromDB($sql,$class = 'stdClass')
+    {
+        $data=[];
+        $resource = $this->connectToDB();
+        mysqli_set_charset($resource,"utf8");
+        $result = mysqli_query($resource,$sql);
 
+        while (NULL !== $row = mysqli_fetch_object($result,$class))
+        {
+            $data[] = $row;
+        }
+        return $data;
+    }
 }
